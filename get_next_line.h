938260e07830fb9f4296ce52b94a8532445ff7e5
May <magrass@student.42heilbronn.de>
2026-05-02 14:29:26 +0200

    updated header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,27 @@
 /*   By: magrass <magrass@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 10:51:29 by magrass           #+#    #+#             */
-/*   Updated: 2026/04/30 03:34:29 by magrass          ###   ########.fr       */
+/*   Updated: 2026/05/02 12:24:14 by magrass          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# ifndef  BUFFER_SIZE
-#  define  BUFFER_SIZE 1024
+# include <stddef.h>
+# include <stdint.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
 # endif
 
+# include <stdlib.h>
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-
-size_t	ft_memcpy(void *dest, const void *src, size_t n);
-
-char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_str_merge(char **merge_into, char *to_merge);
+size_t	ft_strlen(const char *str);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strdup(const char *s);
+ssize_t	find_char(char to_find, char *haystack);
 
 #endif
